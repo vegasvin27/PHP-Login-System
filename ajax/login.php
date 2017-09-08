8e954522f09e46f98a60333bbe8a0acb30e4abfe
Vincent Tripi <vinnyt95@yahoo.com>
2017-09-08 11:17:58 -0400

    Login Via Ajax Part 1

--- ajax/login.php
+++ ajax/login.php
@@ -14,32 +14,20 @@
         $email = Filter::String($_POST['email']);
 
         //Make sure the suer does not exist
-        $findUser = $con->prepare("SELECT user_id FROM users WHERE email = LOWER(:email) LIMIT 1");
+        $findUser = $con->prepare("SELECT user_id, password FROM users WHERE email = LOWER(:email) LIMIT 1");
         $findUser->bindParam(':email', $email, PDO::PARAM_STR);
         $findUser->execute();
 
         if ($findUser->rowCount() == 1) {
-            //User exists
-            //Also check to see if they are able to login
+            //User exists. Try and sign them in
+            $User = $findUser->fetch(PDO::FETCH_ASSOC);
+
             $return['error'] = "You already have an account";
             $return['is logged in'] = false;
         }
         else {
-            //User does not exist. Add now.
-
-            $password = password_hash($_POST['password' ], PASSWORD_DEFAULT);
-
-            $addUser = $con->prepare("INSERT INTO users(email, password) VALUES(LOWER(:email), :password)");
-            $addUser->bindParam(':email', $email, PDO::PARAM_STR);
-             $addUser->bindParam(':password', $password, PDO::PARAM_STR);
-              $addUser->execute();
-
-              $user_id = $con->lastInsertId();
-
-              $_SESSION['user_id'] = (int) $user_id;
-
-              $return['redirect'] = 'dashboard.php?message=welcome';
-              $return['is logged in'] = true;
+            //They need to create a new account
+            $return['error'] = "You do not have an account. <a href='register.php'>Create one now?</a>";
         }
 
 
